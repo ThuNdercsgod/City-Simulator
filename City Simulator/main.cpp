dcbfa53473c261d5672d5951891662daab9e1fd4
ThuNdercsgod <georgi.naoumov@gmail.com>
2025-06-10 20:55:46 +0300

    Fixes

--- City Simulator/main.cpp
+++ City Simulator/main.cpp
@@ -1,9 +1,9 @@
 #include <stdexcept>
 #include <iostream>
 
-#include "command.hpp"
+#include "menu.hpp"
 
-// g++ aspects.cpp building.cpp city.cpp command.cpp dormitory.cpp main.cpp miner.cpp modern.cpp old.cpp programmer.cpp resident.cpp teacher.cpp test.cpp unemployed.cpp -o main.exe
+// g++ aspects.cpp building.cpp city.cpp dormitory.cpp main.cpp menu.cpp miner.cpp modern.cpp old.cpp program.cpp programmer.cpp resident.cpp teacher.cpp unemployed.cpp -o main.exe
 
 int main()
 {
@@ -13,10 +13,25 @@ int main()
     {
         command.menu();
     }
+    catch (const std::bad_alloc &e)
+    {
+        std::cerr << "Memory allocation error!" << std::endl;
+        return 1;
+    }
+    catch (const std::out_of_range &e)
+    {
+        std::cerr << "Memory allocation error!" << std::endl;
+        return 1;
+    }
+    catch (const std::invalid_argument &e)
+    {
+        std::cerr << e.what() << std::endl;
+        return 2;
+    }
     catch (const std::exception &e)
     {
         std::cerr << e.what() << std::endl;
-        return 1;
+        return 3;
     }
 
     return 0;
